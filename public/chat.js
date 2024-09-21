6d61de379164d39d6ba6b0735cb0272d029322a1
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2024-09-21 12:27:32 -0600

    Add files via upload

--- public/chat.js
+++ public/chat.js
@@ -12,11 +12,35 @@ const $message = $('#message'),
 
 // Emit events
 $btn.addEventListener('click', () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const currentTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+
     socket.emit('chat', {
         message: $message.value,
-        handle: $handle.value
+        handle: $handle.value,
+        time: currentTime
     });
+    scrollToBottom();
 });
+
+$message.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const currentTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+
+        socket.emit('chat', {
+            message: $message.value,
+            handle: $handle.value,
+            time: currentTime
+        });
+        $message.value = '';
+    }
+});
+
 
 $message.addEventListener('keypress', () => {
     socket.emit('typing', $handle.value);
@@ -26,14 +50,20 @@ $message.addEventListener('keypress', () => {
 socket.on('chat', (data) => {
     $feedback.innerHTML = '';
     if(data.handle === $handle.value){
-        $output.innerHTML += `<p id="own-message"><strong>${data.handle}: </strong>${data.message}</p>`;
+        $output.innerHTML += `<p id="own-message"><strong>${data.handle}: </strong>${data.message} <span>${data.time}</span></p>`;
     }else{
-        $output.innerHTML += `<p><strong>${data.handle}: </strong>${data.message}</p>`;
+        $output.innerHTML += `<p><strong>${data.handle}: </strong>${data.message} <span>${data.time}</span></p>`;
     }
     $message.value = '';
     $message.focus();
+    scrollToBottom();
 });
 
 socket.on('typing', (data) => {
     $feedback.innerHTML = `<p><em>${data} is typing...</em></p>`;
 });
+
+function scrollToBottom() {
+    const chatWindow = document.getElementById('chat-window');
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+}
